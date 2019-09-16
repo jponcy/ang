a0b5d8e2e0f3eae3198ccecf4b5b3b3b5feca78f
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2019-09-16 11:58:30 +0200

    bbq Q2

--- ang/bbq-finder/src/app/app-routing.module.ts
+++ ang/bbq-finder/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { BbqDetailsComponent } from './bbq/bbq-details/bbq-details.component';
 import { BbqListComponent } from './bbq/bbq-list/bbq-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [{
   path: 'bbq',
@@ -9,6 +10,13 @@ const routes: Routes = [{
 }, {
   path: 'bbq/details',
   component: BbqDetailsComponent
+}, {
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'bbq'
+}, {
+  path: '**',
+  component: NotFoundComponent
 }];
 
 @NgModule({
--- ang/bbq-finder/src/app/bbq/bbq-list/bbq-list.component.ts
+++ ang/bbq-finder/src/app/bbq/bbq-list/bbq-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { BbqService } from '../bbq.service';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Barbecue } from '../barbecue';
+import { BarbecueApiService } from 'src/app/barbecue-api.service';
 
 @Component({
   selector: 'app-bbq-list',
@@ -10,13 +11,26 @@ import { Barbecue } from '../barbecue';
 })
 export class BbqListComponent implements OnInit {
 
-  data$: Observable<Barbecue[]>;
+  barbecues$: Observable<Barbecue[]>;
+  barbecues: Barbecue[];
 
-  constructor(private readonly api: BbqService) { }
+  constructor(private api: BarbecueApiService) { }
 
   ngOnInit() {
+    // Si on a choisi l'observable
+    this.barbecues$ = this.api.getAll();
 
-    this.data$ = this.api.getAll();
+    // Si on a choisi les données
+    this.api
+        .getAll()
+        .subscribe(d => this.barbecues = d, console.error);
+
+    // C'est l'un OU l'autre, pas les deux !
+
+    // A la rigeur, si vraiment on veut les deux, on peut faire ça :
+    // this.barbecues$ = this.api
+    //     .getAll()
+    //     .pipe(tap(d => this.barbecues = d));
+    // Ici au moins, une seule requête à l'API.
   }
-
 }
