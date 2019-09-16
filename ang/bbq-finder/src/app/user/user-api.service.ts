165f8ca9e89123d6902ff6cfc30fc4a98bc5666d
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2019-09-16 14:09:10 +0200

    bbq Q5

--- ang/bbq-finder/src/app/user/user-api.service.ts
+++ ang/bbq-finder/src/app/user/user-api.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { User } from '../bbq/user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const API_URL = environment.apiUrl + 'user/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getAll(): Observable<User[]> {
-    return of([]);
+    return this.http.get<User[]>(API_URL);
   }
 
   create(user: User): Observable<User> {
-    user.id = -42;
+    return this.http.post<User>(API_URL, user);
+  }
 
-    return of(user);
+  delete(id: string|number) {
+    return this.http.delete(`${API_URL}${+id}`);
   }
 }
--- ang/bbq-finder/src/app/user/user.module.ts
+++ ang/bbq-finder/src/app/user/user.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserFormWrongWayComponent } from './user-form-wrong-way/user-form-wrong-way.component';
@@ -7,6 +8,7 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [UserFormWrongWayComponent, UserListComponent],
   imports: [
+    HttpClientModule,
     RouterModule,
     CommonModule
   ],
